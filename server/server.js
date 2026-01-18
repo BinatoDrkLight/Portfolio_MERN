b0e4dd15aa625af02d19a34a524d96c1bfbc22ec
BinatoDrkLight <binatodrklight2@gmail.com>
2026-01-18 19:57:02 +0545

    Added frontend url by BinatoDrkLight at 18 Jan 2026 Sun 7:57 pm

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 4000;
 await connectDB()
 
 //Allow multiple origins
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173', 'https://portfoliomern-one.vercel.app']
 
 //Middleware configuration 
 app.use(express.json());
