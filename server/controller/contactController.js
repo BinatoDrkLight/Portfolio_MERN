01e1e2a8ed90a6da8aff42daf431d9f00c5942de
BinatoDrkLight <binatodrklight2@gmail.com>
2026-01-19 18:47:10 +0545

    added await to sendEmail() by BinatoDrkLight at 19 Jan 2026 Mon 6:47 pm

--- server/controller/contactController.js
+++ server/controller/contactController.js
@@ -30,8 +30,8 @@ export const sendMessage = async (req, res) => {
         }
 
         await Contact.create({...mail})
+        await sendEmail(mail.email, mail.firstName, mail.lastName, mail.message);
         res.json({ success: true, message: "Message sent successfully" })
-        sendEmail(mail.email, mail.firstName, mail.lastName, mail.message);
     } catch (error) {
         console.log(error.message);
         res.json({ success: false, message: error.message })
